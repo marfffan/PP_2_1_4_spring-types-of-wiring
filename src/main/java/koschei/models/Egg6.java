8ec4f4ef63ed146b3b8f86abc15ddb083c2529a9
marfan <marfffan@gmail.com>
2024-12-27 16:08:07 +0300

    Исправления без коментариев 27.12.24

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -5,10 +5,11 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Egg6 {
-    private final Needle7 needle;
 
+
+    private Needle7 needle;
     @Autowired
-    public Egg6(Needle7 needle) {
+    public void setNeedle(Needle7 needle) {
         this.needle = needle;
     }
 
